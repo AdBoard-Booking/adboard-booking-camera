469ea362446fdba37dc1d955edc34c2c278ac6dd
Ankur Kushwaha <akushwaha@adobe.com>
2024-06-07 23:53:31 +0530

    file updated

--- post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git pull origin main
-sudo cp setup_streaming.sh /usr/local/bin/setup_streaming.sh
-sudo cp scan_rtsp.sh /usr/local/bin/scan_rtsp.sh
-sudo chmod +x /usr/local/bin/setup_streaming.sh
-sudo chmod +x /usr/local/bin/scan_rtsp.sh
