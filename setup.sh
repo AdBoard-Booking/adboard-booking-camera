5e1883be8827afaaf43912c8b1bd712b3f4dd825
Ankur kushwaha <ankur.kus1@gmail.com>
2025-02-22 09:10:46 +0530

    setup done

--- setup.sh
+++ setup.sh
@@ -11,15 +11,6 @@ else
     exit 1
 fi
 
-# Check if the traffic/setup.sh script exists and is executable
-if [ -f "traffic/setup.sh" ] && [ -x "traffic/setup.sh" ]; then
-    echo "Running traffic/setup.sh..."
-    sh traffic/setup.sh
-else
-    echo "Error: traffic/setup.sh not found or not executable."
-    exit 1
-fi
-
 cpu_serial=$(cat /proc/cpuinfo | grep "Serial" | awk '{print $3}')
 
 curl -X POST -H "Content-Type: application/json" -d "{\"deviceId\": \"$cpu_serial\"}" https://railway.adboardbooking.com/api/camera/v1/streaming-device
